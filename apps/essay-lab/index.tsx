9f9318410d19260a48970171d6a4c204c26c2f36
huruhumi <taddy.tang@gmail.com>
2026-02-28 22:30:09 +0800

    Cleanup: remove debug build marker

--- apps/essay-lab/index.tsx
+++ apps/essay-lab/index.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-console.log("ESL Master initializing... build v2.1");
+console.log("ESL Master initializing...");
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
